71793b7ab0dd492bba71c209502cda7f6b1c4bc7
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-03-09 20:59:26 +0100

    Implement ValueConverter

--- src/PropertyConverterV7.cs
+++ src/PropertyConverterV7.cs
@@ -10,15 +10,55 @@ namespace Vokseverk {
 	[PropertyValueType(typeof(string))]
 	[PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
 	public class MarkdownTextStringPropertyConverter : PropertyValueConverterBase {
-		// TODO: Change class name above
 		
 		public override bool IsConverter(PublishedPropertyType propertyType) {
-			// TODO: Change alias
 			return propertyType.PropertyEditorAlias.Equals("Vokseverk.MarkdownTextString");
 		}
 		
 		public override object ConvertDataToSource(PublishedPropertyType propertyType, object data, bool preview) {
-			// TODO: Implement
+			var attemptConvertString = data.TryConvertTo<string>();
+			
+			if (attemptConvertString.Success) {
+				return new MarkdownString(attemptConvertString.Result);
+			}
+			
+			return null;
+		}
+		
+		public class MarkdownString {
+			public MarkdownString(string text) {
+				TextValue = text;
+				MarkdownValue = Markdownify(text);
+			}
+			
+			public string TextValue { get; set; }
+			public HtmlString MarkdownValue {
+				get {
+					return Markdownify(TextValue);
+				}
+			}
+			
+			public override HtmlString ToString() {
+				return MarkdownValue;
+			}
+			
+			/// Simple (very) Markdown parsing for headers etc.
+			/// Currently handles *emphasis* and **strong emphasis**
+			private HtmlString Markdownify(string text) {
+				var patternStrong = @"\*\*([^\*]+)\*\*";
+				var replaceStrong = "<strong>$1</strong>";
+
+				var patternEmph = @"\*([^\*]+)\*";
+				var replaceEmph = "<em>$1</em>";
+		
+				var strongRE = new Regex(patternStrong);
+				var emphRE = new Regex(patternEmph);
+		
+				var parsed = strongRE.Replace(text, replaceStrong);
+				parsed = emphRE.Replace(parsed, replaceEmph);
+		
+				return new HtmlString(parsed);
+			}
 		}
 	}
 }
