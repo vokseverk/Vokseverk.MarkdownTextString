987471a40268557e3c0a08e43ff0a57f6dde8419
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-03-10 22:15:56 +0100

    Add MD hint to the textbox
    This gets localized by the controller, since it contains HTML

--- src/propeditor.controller.js
+++ src/propeditor.controller.js
@@ -1,5 +1,15 @@
-angular.module("umbraco").controller("MarkdownTextStringController", function($scope, $element) {
-	// All the calculations possible, maybe...
+angular.module("umbraco").controller("MarkdownTextStringController", function($scope, $element, localizationService) {
+	var strongRE = /(\*\*[^\*]+?\*\*)/;
+	var emphRE = /(\*[^\*]+?\*)/;
+	var hint = $element.querySelector('.vv-md-textstring-hint')
+	
+	localizationService.localize('mdText_hint').then(function(value) {
+		if (value != '' && value.indexOf('*') >= 0) {
+			value = value.replace(strongRE, '<strong>$1</strong>')
+			value = value.replace(emphRE, '<em>$1</em>')
+			hint.innerHTML = value
+		}
+	})
 	
 	// Set the property editor's value...
 	// $scope.model.value = 'Some computed value'
