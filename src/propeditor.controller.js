e0613f506e4b86192104cab8686ae2ab1c63eb93
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-03-11 18:08:25 +0100

    Use `$element#find()` and null-check

--- src/propeditor.controller.js
+++ src/propeditor.controller.js
@@ -1,16 +1,17 @@
 angular.module("umbraco").controller("MarkdownTextStringController", function($scope, $element, localizationService) {
 	var strongRE = /(\*\*[^\*]+?\*\*)/;
 	var emphRE = /(\*[^\*]+?\*)/;
-	var hint = $element.querySelector('.vv-md-textstring-hint')
+	var hint = $element.find('.vv-md-textstring-hint')
 	
-	localizationService.localize('mdText_hint').then(function(value) {
-		if (value != '' && value.indexOf('*') >= 0) {
-			value = value.replace(strongRE, '<strong>$1</strong>')
-			value = value.replace(emphRE, '<em>$1</em>')
-			hint.innerHTML = value
-		}
-	})
-	
-	// Set the property editor's value...
-	// $scope.model.value = 'Some computed value'
+	if (hint != null) {
+		localizationService.localize('mdText_hint').then(function(value) {
+			if (value != '' && value.indexOf('*') >= 0) {
+				value = value.replace('(*)', '(-o-)')
+				value = value.replace(strongRE, '<strong>$1</strong>')
+				value = value.replace(emphRE, '<em>$1</em>')
+				value = value.replace('(-o-)', '(*)')
+				hint.html(value)
+			}
+		})
+	}
 })
